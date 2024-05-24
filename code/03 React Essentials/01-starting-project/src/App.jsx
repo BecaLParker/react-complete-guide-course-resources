783c3aa8c1562a63834c30187847a62f7a01db42
BecaLParker <beca.parker@red-gate.com>
2024-05-24 15:34:32 +0100

    Output dynamic value

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,9 +1,16 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * max + 1);
+}
+
 function Header() {
+    const reactDescription = reactDescriptions[genRandomInt(2)];
     return <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
         <h1>Beca's React Essentials</h1>
         <p>
-            Fundamental React concepts you will need for almost any app you are
+            {reactDescription} React concepts you will need for almost any app you are
             going to build!
         </p>
     </header>;
