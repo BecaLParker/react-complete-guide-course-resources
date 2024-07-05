59d3e6f1565311cc3afbdb8c6cf5c1de9acaa775
BecaLParker <beca.parker@red-gate.com>
2024-07-05 17:12:41 +0100

    change styling conditionally

--- code/03 React Essentials/15-deriving-computing-values/src/App.jsx
+++ code/03 React Essentials/15-deriving-computing-values/src/App.jsx
@@ -52,12 +52,26 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>
+            <TabButton 
+                onSelect={() => handleSelect('components')} 
+                isSelected={selectedTopic === 'components'}>
               Components
             </TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton 
+                onSelect={() => handleSelect('jsx')} 
+                isSelected={selectedTopic === 'jsx'}>
+              JSX
+            </TabButton>
+            <TabButton 
+                onSelect={() => handleSelect('props')}
+                isSelected={selectedTopic === 'props'}>
+              Props
+            </TabButton>
+            <TabButton 
+                onSelect={() => handleSelect('state')}
+                isSelected={selectedTopic === 'state'}>
+              State
+            </TabButton>
           </menu>
           {/*only output this tab content if selectedTopic is not null*/}
           {tabContent}
